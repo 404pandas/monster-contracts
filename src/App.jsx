57cd074cc48e5862d5e6611d8e793cf6d2d380fb
404pandas <mary.panda.jackson@gmail.com>
2025-08-10 10:49:48 -0400

    Home page loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
 
 const App = () => {
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route path='/' element={<MainLayout />}></Route>)
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    )
   );
 
   return <RouterProvider router={router} />;
