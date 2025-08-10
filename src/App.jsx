f434a6abedb2a5f0bcbc13e4cfb781b2a5e211c2
404pandas <mary.panda.jackson@gmail.com>
2025-08-10 12:01:46 -0400

    Typescript because duh

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ import {
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import ContractsPage from "./pages/ContractsPage";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<Home />} />
+        <Route path='contracts' element={<ContractsPage />} />
+        <Route path='*' element={<NotFound />} />
       </Route>
     )
   );
